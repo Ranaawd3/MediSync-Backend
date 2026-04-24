8feca378177d98ef93ffa1c2b479655f13c661fd
Rana Awad <ranaawd30@gmail.com>
2026-04-24 08:28:51 +0300

    fix railway port binding

--- src/MediSync.API/Program.cs
+++ src/MediSync.API/Program.cs
@@ -144,7 +144,8 @@ using (var scope = app.Services.CreateScope())
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.Migrate();
 } 
--------------------
+
+
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Urls.Add($"http://0.0.0.0:{port}");
 
